a5e671672028c881208e32c2f321e2bd3431a4d6
Hirofumi Jinno <hirofumi.jinno@gmail.com>
2021-12-22 20:27:12 -0500

    removed unncessary lines from configuration

--- config.rb
+++ config.rb
@@ -12,24 +12,6 @@ activate :pagination
 
 ignore "/templates/*"
 
-dato.characters.each do |character|
-  proxy "/characters/#{character.slug}.html", "/templates/character.html",
-    locals: { character: character }
-end
-
-dato.seasons.each do |season|
-  proxy "/seasons/#{season.slug}.html", "/templates/season.html",
-    locals: { season: season }
-end
-
-dato.episodes.each do |episode|
-  proxy "/episodes/#{episode.slug}.html", "/templates/episode.html",
-    locals: { episode: episode }
-end
-
-dato.tap do |dato|
-  paginate dato.episodes.sort_by(&:first_aired), "/episodes", "/templates/episodes.html", per_page: 20
-end
 
 configure :build do
   activate :minify_css
