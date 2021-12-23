6ee0ed193b4e0299e79a84cdc48dc1de9bf0505c
Hirofumi Jinno <hirofumi.jinno@gmail.com>
2021-12-22 22:19:00 -0500

    fixing bugs

--- Gemfile
+++ Gemfile
@@ -9,3 +9,20 @@ gem "middleman-paginate"
 gem "html_truncator", "~>0.2"
 gem "redcarpet"
 gem "pry"
+
+gem 'middleman-autoprefixer'
+gem 'middleman-gh-pages'
+gem 'middleman-metaman'
+gem 'rake'
+gem 'rubocop'
+
+gem 'font-awesome-sass', '~> 5.13.0'
+
+group :test do
+  gem 'capybara'
+  gem 'cucumber'
+  gem 'rspec'
+  gem 'selenium-webdriver'
+end
+
+gem "middleman-sprockets"
--- config.rb
+++ config.rb
@@ -2,6 +2,15 @@ page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
+# Load Sass from node_modules
+config[:sass_assets_paths] << File.join(root, 'node_modules')
+
+set :css_dir,    'assets/stylesheets'
+set :fonts_dir,  'assets/fonts'
+set :images_dir, 'assets/images'
+set :js_dir,     'assets/javascripts'
+
+
 configure :development do
   activate :livereload
 end
@@ -17,6 +26,8 @@ configure :build do
   activate :minify_css
   activate :minify_javascript
 end
+
+activate :sprockets
 
 helpers do
   def markdown(text)
